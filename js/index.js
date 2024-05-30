a2752473da6a8e22d4b8b93eb0653c139d47eb61
hongdao <hongru-li@outlook.com>
2024-05-30 16:21:00 +0200

    add calculation function

--- js/index.js
+++ js/index.js
@@ -4,6 +4,27 @@ $buttons.forEach($button => $button.addEventListener("click", showButton));
 
 function showButton(event) {
     const operator = event.target.textContent;
+    const firstOperand = parseFloat(document.querySelector("#first").value)
+    const secondOperand = parseFloat(document.querySelector("#second").value)
+
+    const result = calculate(operator, firstOperand, secondOperand)
+
     document.querySelector("#operator").textContent = operator;
+    document.querySelector("#result").textContent = result;
 };
 
+function calculate(operator, firstOperand, secondOperand) {
+    let result = null
+
+    if (operator === "+") {
+        result = firstOperand + secondOperand
+    } else if (operator === "-") {
+        result = firstOperand - secondOperand
+    } else if (operator === "x") {
+        result = firstOperand * secondOperand
+    } else if (operator === "/") {
+        result = firstOperand / secondOperand
+    }
+
+    return result
+}
