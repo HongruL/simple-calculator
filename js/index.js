d83f89e787b580e8be8329b89d56e3a411a1610a
hongdao <hongru-li@outlook.com>
2024-05-30 16:51:45 +0200

    add clear and go mechanism

--- js/index.js
+++ js/index.js
@@ -17,17 +17,33 @@ class Calculator {
 }
 
 const myCalc = new Calculator();
-const $buttons = document.querySelectorAll("button");
+const $buttons = document.querySelectorAll('button[type="operator"]');
 
 $buttons.forEach($button => $button.addEventListener("click", showButton));
 
 function showButton(event) {
     const operator = event.target.textContent;
-    const firstOperand = parseFloat(document.querySelector("#first").value);
-    const secondOperand = parseFloat(document.querySelector("#second").value);
-
-    const result = myCalc.calculate(operator, firstOperand, secondOperand);
 
     document.querySelector("#operator").textContent = operator;
-    document.querySelector("#result").textContent = result;
 };
+
+// make the calculation
+const $goButton = document.querySelector("#go");
+
+$goButton.addEventListener("click", () => {
+    const firstOperand = parseFloat(document.querySelector("#first").value);
+    const secondOperand = parseFloat(document.querySelector("#second").value);
+    const result = myCalc.calculate(document.querySelector("#operator").textContent, firstOperand, secondOperand);
+
+    document.querySelector("#result").textContent = result;
+})
+
+// clear the input fields
+const $clearButton = document.querySelector("#clear");
+
+$clearButton.addEventListener("click", () => {
+    document.querySelector("#first").value = "";
+    document.querySelector("#second").value = "";
+    document.querySelector("#operator").textContent = "";
+    document.querySelector("#result").textContent = "";
+})
