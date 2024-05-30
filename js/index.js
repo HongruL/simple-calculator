9f886d461d4d1986f04042b8d9036351f983389f
hongdao <hongru-li@outlook.com>
2024-05-30 16:28:20 +0200

    add calculator class re. ECMAScript6

--- js/index.js
+++ js/index.js
@@ -1,30 +1,33 @@
+class Calculator {
+    calculate(operator, firstOperand, secondOperand) {
+        let result = null;
+
+        if (operator === "+") {
+            result = firstOperand + secondOperand
+        } else if (operator === "-") {
+            result = firstOperand - secondOperand
+        } else if (operator === "x") {
+            result = firstOperand * secondOperand
+        } else if (operator === "/") {
+            result = firstOperand / secondOperand
+        }
+
+        return result;
+    }
+}
+
+const myCalc = new Calculator();
 const $buttons = document.querySelectorAll("button");
 
 $buttons.forEach($button => $button.addEventListener("click", showButton));
 
 function showButton(event) {
     const operator = event.target.textContent;
-    const firstOperand = parseFloat(document.querySelector("#first").value)
-    const secondOperand = parseFloat(document.querySelector("#second").value)
+    const firstOperand = parseFloat(document.querySelector("#first").value);
+    const secondOperand = parseFloat(document.querySelector("#second").value);
 
-    const result = calculate(operator, firstOperand, secondOperand)
+    const result = myCalc.calculate(operator, firstOperand, secondOperand);
 
     document.querySelector("#operator").textContent = operator;
     document.querySelector("#result").textContent = result;
 };
-
-function calculate(operator, firstOperand, secondOperand) {
-    let result = null
-
-    if (operator === "+") {
-        result = firstOperand + secondOperand
-    } else if (operator === "-") {
-        result = firstOperand - secondOperand
-    } else if (operator === "x") {
-        result = firstOperand * secondOperand
-    } else if (operator === "/") {
-        result = firstOperand / secondOperand
-    }
-
-    return result
-}
