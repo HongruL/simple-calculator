c0ebdab3453b17dcf0148737438a9dab06373003
hongdao <hongru-li@outlook.com>
2024-05-30 16:11:53 +0200

    add button display

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+const $buttons = document.querySelectorAll("button");
+
+$buttons.forEach($button => $button.addEventListener("click", showButton));
+
+function showButton(event) {
+    const operator = event.target.textContent;
+    document.querySelector("#operator").textContent = operator;
+};
+
